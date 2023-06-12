aa981774f22fad4f1f6180ffb2da1811bae2bf52
Luckyza5023x <113436103+Luckyza5023x@users.noreply.github.com>
2023-06-12 19:23:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const socket = io("/");
+
+$(function () {
+
+    $("#send").click(function () {
+        if ($("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+    $("#chat_message").keydown(function (e) {
+        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+})
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+    res.render("index");
+});
+
+io.on("connection", (socket)=>{
+    socket.on("message",(message)=>{
+        io.emit("createMessage", message);
+    });
+});
+
+socket.on("createMessage",(message)=>{
+    $(".message").append(`
+        <div class="message >
+            <span>${message}</span>
+        </div>
+    `)
+})
+
+server.listen(3030)
